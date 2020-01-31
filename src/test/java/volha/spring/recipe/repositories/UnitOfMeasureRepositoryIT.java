4bc113e7ebea0d6d953e6206415cfe0384379975
Aliot26 <olgakochanowa@gmail.com>
2020-01-31 11:14:55 +0100

    Changed IT test

--- src/test/java/volha/spring/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/volha/spring/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -2,16 +2,17 @@ package volha.spring.recipe.repositories;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import volha.spring.recipe.domain.UnitOfMeasure;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UnitOfMeasureRepositoryIT {
 
